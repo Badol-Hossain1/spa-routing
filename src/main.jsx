01cab52099b12b2682fed60fe024b9a3eea11cce
Badol-Hossain <badolhossain586@gmail.com>
2023-09-29 02:35:23 -0700

    feat: added react router dom in vite project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "about",
+    element: <div> hey from about</div>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
